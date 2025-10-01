58f24d5510e018345f819d105e96e3ad465964ab
Kaique Bahmad <kaiquebahmadt@gmail.com>
2025-10-01 01:06:33 -0300

    first commit

---
+++ src/main/java/kaiquebt/dev/termolivre/TermolivreApplication.java
@@ -0,0 +1,13 @@
+package kaiquebt.dev.termolivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermolivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TermolivreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kaiquebt/dev/termolivre/TermolivreApplicationTests.java
@@ -0,0 +1,13 @@
+package kaiquebt.dev.termolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermolivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
